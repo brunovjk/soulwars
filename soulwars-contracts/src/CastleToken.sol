4a836ff0bb551300f29bfd8d16737b3b3c119eb5
brunovjk <brunovjk@gmail.com>
2023-02-12 19:48:48 -0300

    CastleToken Contract

--- soulwars-contracts/src/CastleToken.sol
+++ soulwars-contracts/src/CastleToken.sol
@@ -1,13 +1,91 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
+import "@thirdweb-dev/contracts/base/ERC721Base.sol";
 
-contract CastleToken is ERC1155LazyMint {
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        address _royaltyRecipient,
-        uint128 _royaltyBps
-    ) ERC1155LazyMint(_name, _symbol, _royaltyRecipient, _royaltyBps) {}
+contract CastleToken is ERC721Base {
+    // Mapping to store the tokens owned by each user
+    mapping(address => uint256[]) public tokenPerUser;
+
+    /**
+     * Constructor
+     * @param _name - the name of the token
+     * @param _symbol - the symbol of the token
+     */
+    constructor(string memory _name, string memory _symbol)
+        ERC721Base(_name, _symbol, msg.sender, 0)
+    {}
+
+    /**
+     * Check the validity of the input string
+     * @param _tokenURI - the string to be checked
+     * @return - true if the input string is valid, false otherwise
+     */
+    function checkInput(string memory _tokenURI) public pure returns (bool) {
+        bytes32 hash = keccak256(bytes(_tokenURI));
+        return
+            hash == keccak256(bytes("1")) ||
+            hash == keccak256(bytes("2")) ||
+            hash == keccak256(bytes("3")) ||
+            hash == keccak256(bytes("4"));
+    }
+
+    /**
+     * Set the token URI for a specific token ID
+     * @param _tokenId - the ID of the token
+     * @param _tokenURI - the new token URI
+     */
+    function setNewTokenURI(uint256 _tokenId, string memory _tokenURI) public {
+        require(checkInput(_tokenURI), "Token URI not valid");
+        _setTokenURI(_tokenId, _tokenURI);
+    }
+
+    /**
+     * Mint a new CastleToken to a user
+     * @param _to - the address of the user to receive the new token
+     */
+    function mintTo(
+        address _to,
+        string memory /*_tokenURI*/
+    ) public override {
+        // Ensure that the sender can only own a maximum of 3 tokens
+        require(
+            tokenPerUser[msg.sender].length <= 3,
+            "Max 3 CastleToken per wallet"
+        );
+
+        // Get the next token ID to mint
+        uint256 _tokenId = nextTokenIdToMint();
+        // Add the token ID to the sender's token list
+        tokenPerUser[msg.sender].push(_tokenId);
+
+        // Set the token URI to 0
+        _setTokenURI(_tokenId, "0");
+        // Mint the token to the user
+        _safeMint(_to, 1, "");
+    }
+
+    // Function to burn a token
+    function burn(uint256 _tokenId) external override {
+        // Loop through the token array
+        for (uint256 i = 1; i < tokenPerUser[msg.sender].length; i++) {
+            // If the token ID matches the one to be burned
+            if (tokenPerUser[msg.sender][i] == _tokenId) {
+                // Shift the remaining tokens in the array to fill the gap
+                for (
+                    uint256 j = i;
+                    j < tokenPerUser[msg.sender].length - 1;
+                    j++
+                ) {
+                    tokenPerUser[msg.sender][j] = tokenPerUser[msg.sender][
+                        j + 1
+                    ];
+                }
+                // Remove the last token in the array
+                tokenPerUser[msg.sender].pop();
+            }
+        }
+        // Burn the token
+        _burn(_tokenId, true);
+    }
 }
--- soulwars-contracts/src/ProfileToken.sol
+++ soulwars-contracts/src/ProfileToken.sol
@@ -4,45 +4,76 @@ pragma solidity ^0.8.0;
 import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
 
 contract ProfileToken is ERC1155LazyMint {
+    // Mapping to store if a user has a token or not
     mapping(address => bool) tokenPerUser;
 
+    /**
+     * Constructor
+     * @param _name - the name of the token
+     * @param _symbol - the symbol of the token
+     */
     constructor(string memory _name, string memory _symbol)
         ERC1155LazyMint(_name, _symbol, msg.sender, 0)
     {}
 
+    /**
+     * Verifies the claim of a token
+     * @param _claimer - the address claiming the token
+     * @param _tokenId - the id of the token being claimed
+     * @param _quantity - the number of tokens being claimed
+     */
     function verifyClaim(
         address _claimer,
         uint256 _tokenId,
         uint256 _quantity
     ) public view override {
+        // Check that the user has not already claimed a token
         require(!tokenPerUser[_claimer], "One ProfileToken per wallet");
+        // Check that the token being claimed is the profile token (id = 0)
         require(_tokenId == 0, "Only ProfileToken");
+        // Check that the user is claiming only one token
         require(_quantity == 1, "Only one token");
     }
 
+    /**
+     * Transfers the token to the user on successful claim
+     * @param _receiver - the address to receive the token
+     */
     function _transferTokensOnClaim(
         address _receiver,
         uint256, /*_tokenId*/
         uint256 /*_quantity*/
     ) internal override {
+        // Set the user as having claimed a token
         tokenPerUser[_receiver] = true;
+        // Mint the token to the user
         _mint(_receiver, 0, 1, "");
     }
 
+    /**
+     * Burns the token for the user
+     * @param _owner - the owner of the token
+     * @param _amount - the number of tokens to burn
+     */
     function burn(
         address _owner,
         uint256, /*_tokenId*/
         uint256 _amount
     ) external override {
+        // Get the caller's address
         address caller = msg.sender;
+        // Set the user as no longer having a token
         tokenPerUser[_owner] = false;
 
+        // Check that the caller is the owner or is approved for the owner
         require(
             caller == _owner || isApprovedForAll[_owner][caller],
             "Unapproved caller"
         );
+        // Check that the owner has enough tokens to burn
         require(balanceOf[_owner][0] >= _amount, "Not enough tokens owned");
 
+        // Burn the tokens
         _burn(_owner, 0, _amount);
     }
 }
